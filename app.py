10b1a0e2715cfaa9e4b565cd2784738ed971c1af
wthoutjc <juancamilorr2012@hotmail.com>
2024-07-13 18:24:47 -0500

    origin fixed

--- app.py
+++ app.py
@@ -22,16 +22,10 @@ def get_application() -> FastAPI:
     app.include_router(transaction.transaction_router, prefix=f"{settings.API_V1_STR}/transactions", tags=["transactions"])
     app.include_router(user.user_router, prefix=f"{settings.API_V1_STR}/user", tags=["user"])
 
-    origins = [
-        "http://localhost",
-        "http://localhost:3000",
-        "*"
-    ]
-
     app.add_middleware(
         CORSMiddleware,
+        allow_origins=["*"],
         allow_credentials=True,
-        allow_origins=origins,
         allow_methods=["*"],
         allow_headers=["*"],
         expose_headers=["*"],
